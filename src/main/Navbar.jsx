9dd7568c91f9e7950bf1e430d3bad196d6acf322
Alexandru2401 <a.toma231@yahoo.com>
2024-06-21 18:32:06 +0300

    responsive-navbar

--- src/main/Navbar.jsx
+++ src/main/Navbar.jsx
@@ -1,103 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 function Navbar() {
   const [nav, setNav] = useState(false);
 
-  const openNav = () => {
-    setNav(!nav);
-  };
-
-  const closeMenu = () => {
-    setNav(false);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 500) {
-        closeMenu();
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (window.innerWidth <= 1200) {
-      closeMenu();
-    }
-  }, []);
   return (
-    <nav className={`navbar ${nav ? "active" : ""}`}>
+    <nav>
       <div>
         <img src={logo} className="logo" />
       </div>
-      <a className={`nav-hamburger ${nav ? "active" : ""}`} onClick={openNav}>
-        <span className="nav-hamburger-line"></span>
-        <span className="nav-hamburger-line"></span>
-        <span className="nav-hamburger-line"></span>
-      </a>
-      <div className={`navbar-items ${nav ? "active" : ""}`}>
-        <ul>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="/"
-              className="navbar-content"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="/ProjectsPage"
-              className="navbar-content"
-            >
-              Portofolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="/AboutMePage"
-              className="navbar-content"
-            >
-              About Me
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <Link
-        activeClass="navbar-active-content"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-        to="/ContactMe"
-        className="btn btn-outline-primary"
+
+      <ul
+        className={nav ? "list-container-mobile show" : "list-container"}
+        onClick={() => {
+          setNav(false);
+        }}
       >
-        Contact me
-      </Link>
+        <li>
+          <Link to="/" className="navbar-content">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/ProjectsPage" className="navbar-content">
+            Portofolio
+          </Link>
+        </li>
+        <li>
+          <Link to="/AboutMePage" className="navbar-content">
+            About Me
+          </Link>
+        </li>
+        <li>
+          <Link to="/ContactMe" className="btn btn-outline-primary">
+            Contact me
+          </Link>
+        </li>
+      </ul>
+
+      <button
+        className="nav-btn"
+        onClick={() => {
+          setNav(!nav);
+        }}
+      >
+        {" "}
+        {nav ? <CloseIcon /> : <MenuIcon />}
+      </button>
     </nav>
   );
 }
